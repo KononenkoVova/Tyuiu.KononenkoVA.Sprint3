c4fbe558e1052fc846ab1ff86f1ffe822ad641ca
Владимир Кононенко <Tyuiu.KononenkoVA@outlook.com>
2025-10-25 21:45:10 +0300

    Task2

---
+++ Tyuiu.KononenkoVA.Sprint3.Task2.V5.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.KononenkoVA.Sprint3.Task2.V5.Lib
+{
+    public class DataService : ISprint3Task2V5
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double sumSeries = 0;
+
+            while (startValue <= stopValue)
+            {
+                sumSeries += (Math.Pow(value, startValue) + 1) * Math.Cos(startValue);
+                startValue++;
+            }
+
+            return sumSeries;
+        }
+    }
+}
